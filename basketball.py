749351b7626a176680f7b298549da29517d2137a
pierre0210 <conandoyle.0210@gmail.com>
2021-11-22 17:35:47 +0800

    Q4 finished

--- basketball.py
+++ basketball.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class ball:
-    def __init__(self, mass, height=3.05, ballD=0.24, rimD=0.4572, cw=0, airDensity=0, g=9.8):
+    def __init__(self, mass, height=3.05, ballD=0.24, rimD=0.4572, cw=0, airDensity=0, g=9.8, omega=0.0):
         self.mass = mass
         self.height = height
         self.ballD = ballD
@@ -9,6 +9,7 @@ class ball:
         self.cw = cw
         self.density = airDensity
         self.g = g
+        self.omega = omega
         self.xpos = 0
         self.ypos = 2
         self.dt = 0.005
@@ -30,11 +31,12 @@ class ball:
         self.od = self.distance(self.xpos, self.ypos)
         while True:
             df = self.dragForce(self.vx, self.vy)
+            lf = self.liftForce(self.vx, self.vy)
             self.xpos += self.vx*self.dt
             self.ypos += self.vy*self.dt
             #print(df)
-            self.vx += -df[0]*self.dt/self.mass
-            self.vy += -self.g*self.dt-df[1]*self.dt/self.mass            
+            self.vx += -df[0]*self.dt/self.mass-lf[0]*self.dt/self.mass
+            self.vy += -self.g*self.dt-df[1]*self.dt/self.mass-lf[1]*self.dt/self.mass          
             #print(str(self.vx)+" "+str(self.vy))
             xlist.append(self.xpos)
             ylist.append(self.ypos)
@@ -62,6 +64,13 @@ class ball:
         #curAngle = np.arctan(vy/vx)
         dragForce = 0.5*self.cw*self.density*((self.ballD/2)**2)*np.pi*(vx**2+vy**2)
         return [dragForce*vx/((vx**2+vy**2)**0.5), dragForce*vy/((vx**2+vy**2)**0.5)]
+
+    def liftForce(self, vx, vy):
+        if self.omega == 0:
+            return [0, 0]
+        cl = 1/(2+(vx**2+vy**2)**0.5/((self.ballD/2)*self.omega))
+        liftForce = 0.5*cl*((self.ballD/2)**2)*np.pi*(vx**2+vy**2)
+        return [liftForce*vy/((vx**2+vy**2)**0.5), -liftForce*vx/((vx**2+vy**2)**0.5)]
 
     def error(self, bx, by):
         slope = -np.cos(self.angle*np.pi/180.0)/np.sin(self.angle*np.pi/180.0)
--- sim.py
+++ sim.py
@@ -2,72 +2,135 @@ import basketball
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit, minimize
+from mpl_toolkits import mplot3d
 
 def altRange(start, end, step):
     return np.arange(start/step, end/step)*step
 
-def func(v, a, b, c):
-    return a*v**2+b*v+c
+def func(x, a, b, c):
+    return a*x**2+b*x+c
 
 curveX = np.linspace(7, 17, 1000)
-'''
+
 #Question 1
 print("Question 1")
 ball1 = basketball.ball(mass=0.62)
 vList = []
 testDistance = [6.25, 7.6, 14]
+testAngle = [35, 45, 55]
+upperV = []
+lowerV = []
+angleX = np.linspace(30, 60, 3000)
 
 for d in testDistance:
-    distanceToRim = []
-    for i in altRange(7.0, 17.0, 0.01):
-        i = round(i, 2)
-        result = ball1.shoot(i, 45, d)
-        distanceToRim.append(result[3])
-        if result[2]:
-            vList.append(i)
-            #plt.scatter(result[0], result[1])
-    vList = [vList[0], vList[-1]]
-    print(vList)
-    vList = []
-    plt.plot(curveX, distanceToRim, label=str(d)+" m without air resistance")
+    for a in testAngle:
+        for i in altRange(7, 17, 0.01):
+            result = ball1.shoot(i, a, d)
+            if result[2]:
+                vList.append(i)
+        lowerV.append(vList[0])
+        upperV.append(vList[-1])
+        vList = []
+    lowerpopt, lowerpcov = curve_fit(func, testAngle, lowerV)
+    print(str(d)+" m "+str(func(45, *lowerpopt)))
+    #minimumObj = minimize(func, 45, (lowerpopt[0], lowerpopt[1], lowerpopt[2]))
+    #print(str(d)+" m "+str(minimumObj.x)+" "+str(minimumObj.fun))
+    plt.plot(angleX, func(angleX, *lowerpopt), label=str(d)+" m lower without air resistance")
+    #upperpopt, upperpcov = curve_fit(func, testAngle, upperV)
+    #plt.plot(angleX, func(angleX, *upperpopt), label=str(d)+" m upper")
+    lowerV = []
+    upperV = []
+    
 
-#plt.grid()
-#plt.show()
-
+plt.xlabel("angle (degree)")
+plt.ylabel("initial velocity (m/s)")
+plt.legend()
+plt.grid()
+plt.show()
 
 #Question 2
 print("Question 2")
 ball2 = basketball.ball(mass=0.62, cw=0.5, airDensity=1.14)
-testSpeed = [5, 11, 17]
-testDistance = [6.25, 7.6, 14]
-
 vList = []
+testDistance = [6.25, 7.6, 14]
+testAngle = [35, 45, 55]
+upperV = []
+lowerV = []
+angleX = np.linspace(30, 60, 3000)
 
 for d in testDistance:
-    distanceToRim = []
-    for i in altRange(7, 17, 0.01):
-        result = ball2.shoot(i, 45, d)
-        distanceToRim.append(result[3])
-        if result[2]:
-            vList.append(i)
-    vList = [vList[0], vList[-1]]
-    print(vList)
-    vList = []
-    #popt, pcov = curve_fit(func, curveX, distanceToRim)
-    #plt.plot(curveX, func(curveX, *popt))
-    plt.plot(curveX, distanceToRim, label=str(d)+" m with air resistance")
-    #solve = fsolve(func, [0, ball2.getRimRange()], args=(popt[0], popt[1], popt[2]))
-    #print(solve)
-
-#plt.scatter(testSpeed, distanceToRim)
+    for a in testAngle:
+        for i in altRange(7, 17, 0.01):
+            result = ball2.shoot(i, a, d)
+            if result[2]:
+                vList.append(i)
+        lowerV.append(vList[0])
+        upperV.append(vList[-1])
+        vList = []
+    lowerpopt, lowerpcov = curve_fit(func, testAngle, lowerV)
+    print(str(d)+" m "+str(func(45, *lowerpopt)))
+    #minimumObj = minimize(func, 45, (lowerpopt[0], lowerpopt[1], lowerpopt[2]))
+    #print(str(d)+" m "+str(minimumObj.x)+" "+str(minimumObj.fun))
+    plt.plot(angleX, func(angleX, *lowerpopt), label=str(d)+" m lower with air resistance")
+    #upperpopt, upperpcov = curve_fit(func, testAngle, upperV)
+    #plt.plot(angleX, func(angleX, *upperpopt), label=str(d)+" m upper")
+    lowerV = []
+    upperV = []
+    
+plt.xlabel("angle (degree)")
+plt.ylabel("initial velocity (m/s)")
 plt.legend()
 plt.grid()
 plt.show()
-'''
 
 #Question 3
+#fig = plt.figure()
+#ax = plt.axes(projection='3d')
+print("Question 3")
 angleX = np.linspace(30, 60, 3000)
 ball3 = basketball.ball(mass=0.62, cw=0.5, airDensity=1.14)
+vList = []
+bestAngle = 0
+lowestV = 20.0
+
+upperV = []
+lowerV = []
+
+testDistance = [6.25, 7.6, 14]
+#testDistance = np.linspace(6.25, 14, 80)
+testAngle = [45, 50, 55]
+
+for d in testDistance:
+    for a in testAngle:
+        for i in altRange(7, 17, 0.01):
+            result = ball3.shoot(i, a, d)
+            if result[2]:
+                vList.append(i)
+        lowerV.append(vList[0])
+        upperV.append(vList[-1])
+        vList = []
+    lowerpopt, lowerpcov = curve_fit(func, testAngle, lowerV)
+    #print(str(d)+" m "+str(func(45, *lowerpopt)))
+    minimumObj = minimize(func, 45, (lowerpopt[0], lowerpopt[1], lowerpopt[2]))
+    print(str(d)+" m "+str(minimumObj.x)+" "+str(minimumObj.fun))
+    plt.scatter(minimumObj.x, minimumObj.fun)
+    plt.plot(angleX, func(angleX, *lowerpopt), label=str(d)+" m lower")
+    #upperpopt, upperpcov = curve_fit(func, testAngle, upperV)
+    #plt.plot(angleX, func(angleX, *upperpopt), label=str(d)+" m upper")
+    lowerV = []
+    upperV = []
+
+plt.xlabel("angle (degree)")
+plt.ylabel("initial velocity (m/s)")
+plt.legend()
+plt.grid()
+plt.show()
+
+
+#Question 4
+print("Question 4")
+angleX = np.linspace(30, 60, 3000)
+ball4 = basketball.ball(mass=0.62, cw=0.5, airDensity=1.14, omega=6)
 vList = []
 bestAngle = 0
 lowestV = 20.0
@@ -81,21 +144,26 @@ testAngle = [45, 50, 55]
 for d in testDistance:
     for a in testAngle:
         for i in altRange(7, 17, 0.01):
-            result = ball3.shoot(i, a, d)
+            result = ball4.shoot(i, a, d)
             if result[2]:
                 vList.append(i)
         lowerV.append(vList[0])
         upperV.append(vList[-1])
         vList = []
     lowerpopt, lowerpcov = curve_fit(func, testAngle, lowerV)
+    print(str(d)+" m "+str(func(45, *lowerpopt)))
     minimumObj = minimize(func, 45, (lowerpopt[0], lowerpopt[1], lowerpopt[2]))
     print(str(d)+" m "+str(minimumObj.x)+" "+str(minimumObj.fun))
-    plt.plot(angleX, func(angleX, *lowerpopt), label=str(d)+" m lower")
-    upperpopt, upperpcov = curve_fit(func, testAngle, upperV)
-    plt.plot(angleX, func(angleX, *upperpopt), label=str(d)+" m upper")
+    plt.scatter(minimumObj.x, minimumObj.fun)
+    plt.plot(angleX, func(angleX, *lowerpopt), label=str(d)+" m lower with spin")
+    #upperpopt, upperpcov = curve_fit(func, testAngle, upperV)
+    #plt.plot(angleX, func(angleX, *upperpopt), label=str(d)+" m upper with spin")
     lowerV = []
     upperV = []
 
+
+plt.xlabel("angle (degree)")
+plt.ylabel("initial velocity (m/s)")
 plt.legend()
 plt.grid()
 plt.show()
